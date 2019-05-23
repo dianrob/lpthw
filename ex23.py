94e31dbbf96b51ba40df5c8b4ddfc9859f8b605e
dianrob <robert.synofzik@gmail.com>
2019-05-23 15:33:39 +0200

    changes according to pep-8

--- ex23.py
+++ ex23.py
@@ -2,6 +2,7 @@ import sys
 
 script, encoding, error = sys.argv
 
+
 def main(language_file, encoding, errors):
     line = language_file.readline()
 
@@ -9,12 +10,12 @@ def main(language_file, encoding, errors):
         print_line(line, encoding, errors)
         return main(language_file, encoding, errors)
 
+
 def print_line(line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors)
     cooked_string = raw_bytes.decode(encoding, errors=errors)
     print(raw_bytes, "<=====>", cooked_string)
-
 
 
 languages = open("languages.txt", encoding="utf-8")
